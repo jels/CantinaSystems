31945b6647065e65f124f5aa728172c0b04bab05
jels <jels1991@gmail.com>
2023-05-23 19:29:12 -0400

    v

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   //apiURL: 'http://localhost/cantinasystems/src/app',
   //apiURL: 'http://localhost/cantinasystems',
-  apiURL: 'https://csystems.adzconsultora.com/',
+  apiURL: 'https://csystems.adzconsultora.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   //apiURL: 'http://localhost/cantinasystems/src/app',
   //apiURL: 'http://localhost/cantinasystems',
-  apiURL: 'https://csystems.adzconsultora.com/',
+  apiURL: 'https://csystems.adzconsultora.com',
 };
