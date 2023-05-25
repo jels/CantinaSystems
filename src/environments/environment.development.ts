cccf696604ce96a7e833559649ae1a737689adbe
jels <jels1991@gmail.com>
2023-05-24 22:20:58 -0400

    .

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   //apiURL: 'http://localhost/cantinasystems/src/app',
   //apiURL: 'http://localhost/cantinasystems',
   //apiURL: 'https://csystems.adzconsultora.com',
-  apiURL: 'https://adzconsultora.com/apic/comserv',
+  apiURL: 'https://adzconsultora.com/api/comserv',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   //apiURL: 'http://localhost/cantinasystems/src/app',
   //apiURL: 'http://localhost/cantinasystems',
   //apiURL: 'https://csystems.adzconsultora.com',
-  apiURL: 'https://adzconsultora.com/apic/comserv',
+  apiURL: 'https://adzconsultora.com/api/comserv',
 };
